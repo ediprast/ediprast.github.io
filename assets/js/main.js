51aa2460f73b0d21634346a62b943cd563ad49ff
ediprast <ediprastyo96@gmail.com>
2022-03-16 13:09:36 +0700

    change animation duration

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,29 +125,29 @@ const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 2500,
-    delay: 400,
+    delay: 300,
     reset: true,
 })
 
 sr.reveal(`.home-data`)
-sr.reveal(`.home-image`, { delay: 700 })
+sr.reveal(`.home-image`, { delay: 600 })
 sr.reveal(`.home-social`, { delay: 900, origin: 'bottom' })
 
 const srServices = ScrollReveal({
     origin: 'bottom',
     distance: '100px',
-    duration: 2500,
-    delay: 200,
+    duration: 2000,
+    delay: 100,
     reset: true,
 })
 
 sr.reveal(`.reveal-ui`)
-sr.reveal(`.reveal-web`, { delay: 500 })
-sr.reveal(`.reveal-app`, { delay: 800 })
+sr.reveal(`.reveal-web`, { delay: 400 })
+sr.reveal(`.reveal-app`, { delay: 700 })
 
 sr.reveal(`.reveal-email`)
-sr.reveal(`.reveal-tele`, { delay: 500 })
-sr.reveal(`.reveal-mess`, { delay: 800 })
+sr.reveal(`.reveal-tele`, { delay: 400 })
+sr.reveal(`.reveal-mess`, { delay: 700 })
 
 /*=============== TILT ANIMATION ===============*/
 
